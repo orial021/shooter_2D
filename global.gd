f4eaf908f66672fa96a9ea147addc588b9593631
Jairo Belen <jairo02193@gmail.com>
2024-04-26 12:21:17 -0400

    feat (estructura inicial II)

--- global.gd
+++ global.gd
@@ -3,3 +3,11 @@ extends Node
 var score : int = 0
 var credits : int = 0
 
+
+
+var axis : Vector2
+
+func get_axis() -> Vector2:
+	axis.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_right"))
+	axis.y = int(Input.is_action_pressed("ui_up")) - int(Input.is_action_pressed("ui_down"))
+	return axis.normalized()
